361e9e04f410305808d4833b855232302377cb66
Carlos Ramirez <crramirez@gmail.com>
2021-02-22 00:51:08 +0100

    More instance memory

--- create-targz.sh
+++ create-targz.sh
@@ -37,7 +37,7 @@ curl $KSFILE -o install.ks
 rm -f /var/tmp/install.tar.xz
 
 #build intermediary rootfs tar
-sudo livemedia-creator --make-tar --iso="${INSTALLISO}" --image-name=install.tar.xz --ks=install.ks --releasever "7" -ram 512
+sudo livemedia-creator --make-tar --iso="${INSTALLISO}" --image-name=install.tar.xz --ks=install.ks --releasever "7"
 
 #open up the tar into our build directory
 tar -xvf /var/tmp/install.tar.xz -C "${BUILDDIR}"
