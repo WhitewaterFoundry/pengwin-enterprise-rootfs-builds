5fb5eab571fe45dbb56ec2dcd093fa646f043653
Carlos Rafael Ramirez <crramirez@users.noreply.github.com>
2021-10-13 13:10:36 +0200

    Improve Upgrade.sh

--- linux_files/upgrade.sh
+++ linux_files/upgrade.sh
@@ -1,30 +1,20 @@
 #!/bin/bash
 
+base_url="https://raw.githubusercontent.com/WhitewaterFoundry/pengwin-enterprise-rootfs-builds/master"
+sudo curl -L -f "${base_url}/linux_files/upgrade.sh" -o /usr/local/bin/upgrade.sh
+sudo chmod +x /usr/local/bin/upgrade.sh
+
 # If WSL1 and fake sudo is installed, then execute the script with su
 if [[ -z "${WSL2}" && "$(sudo bash -c 'echo "$(whoami)"')" != "root" ]]; then
   su -c /usr/local/bin/upgrade.sh
   exit 0
 fi
 
-base_url="https://raw.githubusercontent.com/WhitewaterFoundry/pengwin-enterprise-rootfs-builds/master"
-sha256sum /usr/local/bin/upgrade.sh >/tmp/sum.txt
-sudo curl -L -f "${base_url}/linux_files/upgrade.sh" -o /usr/local/bin/upgrade.sh
-sudo chmod +x /usr/local/bin/upgrade.sh
-sha256sum -c /tmp/sum.txt
-
-changed=$?
-rm -r /tmp/sum.txt
-
-# the script has changed? run the newer one
-if [ ${changed} -eq 1 ]; then
-  echo 'The script has been updated please, run it again with $ upgrade.sh'
-  
-  exit 0
-fi
-
 echo -n -e '\033]9;4;3;100\033\\'
 
-sudo yum -y update
+sudo rm -f /var/lib/rpm/.rpm.lock
+sudo yum -y update --nogpgcheck
+sudo rm -f /var/lib/rpm/.rpm.lock
 
 # Update the release and main startup script files
 sudo curl -L -f "${base_url}/linux_files/00-wle.sh" -o /etc/profile.d/00-wle.sh
