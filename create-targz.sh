030b89534a2c040f84007d17bf33d689b2534731
Carlos Ramirez <crramirez@gmail.com>
2021-10-16 17:25:52 +0200

    improve the create-targz script

--- create-targz.sh
+++ create-targz.sh
@@ -32,16 +32,16 @@ ks_file="https://raw.githubusercontent.com/WhitewaterFoundry/sig-cloud-instance-
 cd "$tmp_dir"
 
 echo "##[section] make sure we are up to date"
-yum -y update
+dnf -y update
 
 echo "##[section] get livemedia-creator dependencies"
-yum -y install libvirt lorax virt-install libvirt-daemon-config-network libvirt-daemon-kvm libvirt-daemon-driver-qemu
+dnf -y install libvirt lorax virt-install libvirt-daemon-config-network libvirt-daemon-kvm libvirt-daemon-driver-qemu bc
 
 #get anaconda dependencies
-#yum -y install anaconda anaconda-tui
+#dnf -y install anaconda anaconda-tui
 
 echo "##[section] restart libvirtd for good measure"
-systemctl restart libvirtd
+systemctl restart libvirtd || echo "Running without SystemD"
 
 echo "##[section] download enterprise boot ISO"
 if [[ ! -f ${install_iso} ]]; then
@@ -53,10 +53,14 @@ curl $ks_file -o install.ks
 rm -f "${install_tar_gz}"
 
 echo "##[section] build intermediary rootfs tar"
-livemedia-creator --make-tar --iso="${install_iso}" --image-name=install.tar.gz --ks=install.ks --releasever "8" --vcpus 4 --ram=4096 --compression gzip --tmp "${dest_dir}"
+processor_count=$(grep -c "processor.*:" /proc/cpuinfo)
+ram=$(free -m | sed -n "sA\(Mem: *\)\([0-9]*\)\(.*\)A\2 / 2Ap" | bc -l | cut -d'.' -f1)
+livemedia-creator --make-tar --iso="${install_iso}" --image-name=install.tar.gz --ks=install.ks --releasever "8" --vcpus ${processor_count} --ram=${ram} --compression gzip --tmp "${dest_dir}"
+unset processor_count
+unset ram
 
 echo "##[section] open up the tar into our build directory"
-tar -xvf "${install_tar_gz}" -C "${build_dir}"
+tar -xf "${install_tar_gz}" -C "${build_dir}"
 
 echo "##[section] copy some custom files into our build directory"
 cp "${origin_dir}"/linux_files/wsl.conf "${build_dir}"/etc/wsl.conf
