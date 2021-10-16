47b7f6da2dbb44e03cd6e8bec7ebb609dbf42b70
Carlos Ramirez <crramirez@gmail.com>
2021-10-17 00:31:23 +0200

    fix install iso location

--- create-targz.sh
+++ create-targz.sh
@@ -45,7 +45,7 @@ systemctl restart libvirtd || echo "Running without SystemD"
 
 echo "##[section] download enterprise boot ISO"
 if [[ ! -f ${install_iso} ]]; then
-  cp "${boot_iso}" "${install_iso}"
+  curl "${boot_iso}" -o "${install_iso}"
 fi
 echo "##[section] download enterprise Docker kickstart file"
 curl $ks_file -o install.ks
