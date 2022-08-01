b09234ddddf92b8216acf64a200b53148defbd6f
Carlos Ramirez <crramirez@gmail.com>
2022-08-01 22:10:31 +0200

    upgrade.sh installs support for SystemD

--- linux_files/upgrade.sh
+++ linux_files/upgrade.sh
@@ -4,6 +4,12 @@ base_url="https://raw.githubusercontent.com/WhitewaterFoundry/pengwin-enterprise
 sudo curl -L -f "${base_url}/linux_files/upgrade.sh" -o /usr/local/bin/upgrade.sh
 sudo chmod +x /usr/local/bin/upgrade.sh
 
+# Do not change above this line to avoid update errors
+
+if [[ ! -L /usr/local/bin/update.sh  ]]; then
+  sudo ln -s /usr/local/bin/upgrade.sh /usr/local/bin/update.sh
+fi
+
 # If WSL1 and fake sudo is installed, then execute the script with su
 if [[ -z "${WSL2}" && "$(sudo bash -c 'echo "$(whoami)"')" != "root" ]]; then
   su -c /usr/local/bin/upgrade.sh
@@ -26,18 +32,30 @@ sudo curl -L -f "${base_url}/linux_files/local.conf" -o /etc/fonts/local.conf
 # Install mesa
 source /etc/os-release
 if [[ -n ${WAYLAND_DISPLAY} && ${VERSION_ID} == '8.5' && $( sudo dnf info --installed mesa-libGL | grep -c '21.1.5-wsl' ) == 0 ]]; then
-  sudo dnf -y install 'dnf-command(versionlock)'
-  sudo dnf versionlock delete mesa-dri-drivers mesa-libGL mesa-filesystem mesa-libglapi
+  sudo yum -y install 'dnf-command(versionlock)'
+  sudo yum versionlock delete mesa-dri-drivers mesa-libGL mesa-filesystem mesa-libglapi
   curl -s https://packagecloud.io/install/repositories/whitewaterfoundry/pengwin-enterprise/script.rpm.sh | sudo bash
-  sudo dnf -y install --allowerasing --nogpgcheck mesa-dri-drivers-21.1.5-wsl.el8 mesa-libGL-21.1.5-wsl.el8 glx-utils
-  sudo dnf versionlock add mesa-dri-drivers mesa-libGL mesa-filesystem mesa-libglapi
+  sudo yum -y install --allowerasing --nogpgcheck mesa-dri-drivers-21.1.5-wsl.el8 mesa-libGL-21.1.5-wsl.el8 glx-utils
+  sudo yum versionlock add mesa-dri-drivers mesa-libGL mesa-filesystem mesa-libglapi
 fi
 
 # Install support for SystemD
 
 # if machinectl is not installed then install it
 if (! command -v machinectl >/dev/null 2>&1); then
-  sudo dnf -y install systemd-container
+  sudo yum -y install systemd-container
 fi
+
+# Upgrade Systemd
+sudo curl -L -f "${base_url}/linux_files/start-systemd.sudoers" -o /etc/sudoers.d/start-systemd
+sudo curl -L -f "${base_url}/linux_files/start-systemd.sh" -o /usr/local/bin/start-systemd
+sudo curl -L -f "${base_url}/linux_files/wsl2-xwayland.service" -o /etc/systemd/system/wsl2-xwayland.service
+sudo curl -L -f "${base_url}/linux_files/wsl2-xwayland.socket" -o /etc/systemd/system/wsl2-xwayland.socket
+sudo ln -sf ../wsl2-xwayland.socket /etc/systemd/system/sockets.target.wants/
+
+
+sudo curl -L -f "${base_url}/linux_files/systemctl3.py" -o /usr/local/bin/wslsystemctl
+sudo chmod u+x /usr/local/bin/start-systemd
+sudo chmod +x /usr/local/bin/wslsystemctl
 
 echo -n -e '\033]9;4;0;100\033\\'
