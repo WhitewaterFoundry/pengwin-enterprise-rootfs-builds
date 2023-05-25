2db1d80a28d4c10ffd06cc9e14be673c0d08c5c7
Carlos Ramirez <crramirez@gmail.com>
2023-05-25 17:34:55 +0200

    Update rocky 8

--- linux_files/upgrade.sh
+++ linux_files/upgrade.sh
@@ -22,12 +22,27 @@ sudo rm -f /var/lib/rpm/.rpm.lock
 
 # Update mesa
 source /etc/os-release
-if [[ ${VERSION_ID} == "8.7"* && $( sudo dnf info --installed mesa-libGL | grep -c '22.1.5-wsl' ) == 0 ]]; then
-  sudo yum -y install 'dnf-command(versionlock)'
-  sudo yum versionlock delete mesa-dri-drivers mesa-libGL mesa-filesystem mesa-libglapi
-  curl -s https://packagecloud.io/install/repositories/whitewaterfoundry/pengwin-enterprise/script.rpm.sh | sudo bash
-  sudo yum -y install --allowerasing --nogpgcheck mesa-dri-drivers-22.1.5-wsl.el8 mesa-libGL-22.1.5-wsl.el8 glx-utils
-  sudo yum versionlock add mesa-dri-drivers mesa-libGL mesa-filesystem mesa-libglapi
+
+declare -a mesa_version=('22.3.0-wsl2' '22.3.0-wsl2')
+declare -a target_version=('8' '9')
+declare -i length=${#mesa_version[@]}
+
+for (( i = 0; i < length; i++ )); do
+
+  if [[ ${VERSION_ID} == ${target_version[i]}* && $(sudo dnf info --installed mesa-libGL | grep -c "${mesa_version[i]}") == 0 ]]; then
+    sudo dnf -y install 'dnf-command(versionlock)'
+    sudo dnf versionlock delete mesa-dri-drivers mesa-libGL mesa-filesystem mesa-libglapi mesa-vdpau-drivers mesa-libEGL mesa-libgbm mesa-libxatracker mesa-vulkan-drivers
+    curl -s https://packagecloud.io/install/repositories/whitewaterfoundry/pengwin-enterprise/script.rpm.sh | sudo bash
+    sudo dnf -y install --allowerasing --nogpgcheck mesa-dri-drivers-"${mesa_version[i]}".el"${target_version[i]}" mesa-libGL-"${mesa_version[i]}".el"${target_version[i]}" mesa-vdpau-drivers-"${mesa_version[i]}".el"${target_version[i]}" mesa-libEGL-"${mesa_version[i]}".el"${target_version[i]}" mesa-libgbm-"${mesa_version[i]}".el"${target_version[i]}" mesa-libxatracker-"${mesa_version[i]}".el"${target_version[i]}" mesa-vulkan-drivers-"${mesa_version[i]}".el"${target_version[i]}" glx-utils
+    sudo dnf -y install --allowerasing --nogpgcheck libva-utils
+    sudo dnf versionlock add mesa-dri-drivers mesa-libGL mesa-filesystem mesa-libglapi mesa-vdpau-drivers mesa-libEGL mesa-libgbm mesa-libxatracker mesa-vulkan-drivers
+  fi
+done
+
+if [[ $(id | grep -c video) == 0 ]]; then
+  sudo /usr/sbin/groupadd -g 44 wsl-video
+  sudo /usr/sbin/usermod -aG wsl-video "$(whoami)"
+  sudo /usr/sbin/usermod -aG video "$(whoami)"
 fi
 
 sudo yum -y update
