3276dd9575351872d05d6c8fe2af79ed31549b0a
Carlos Ramirez <crramirez@gmail.com>
2023-05-28 17:59:26 +0200

    Update root RHEL 9

--- linux_files/00-wle.sh
+++ linux_files/00-wle.sh
@@ -93,7 +93,7 @@ main() {
 
   # speed up some GUI apps like gedit
   export NO_AT_BRIDGE=1
-
+  export PS1='\[\033]133;D;$?\]\[\033\\\033]133;A\033\\\][\u@\h \W]\$ \[\033]133;B\033\\\]'
   export TERM=xterm-256color
 
   # Fix 'clear' scrolling issues
@@ -103,6 +103,10 @@ main() {
   alias ll='ls -al'
   alias winget='powershell.exe winget'
   alias wsl='wsl.exe'
+
+  #Setup video acceleration
+  export VDPAU_DRIVER=d3d12
+  export LIBVA_DRIVER_NAME=d3d12
 
   # Fix $PATH for Systemd
   SYSTEMD_PID="$(ps -C systemd -o pid= | head -n1)"
