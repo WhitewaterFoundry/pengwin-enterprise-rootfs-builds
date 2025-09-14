ecd9dc94a599e0e3655ce0f769803ed5c0a4936b
Carlos Ramirez <crramirez@gmail.com>
2025-09-14 21:19:07 +0200

    Refine WSL setup scripts: Update bash prompt logic, improve Mesa/LLVM handling, and fix systemd PID fallback
    Modified `bash-prompt-wsl.sh` to add conditional prompt updates for clarity and switched to `sh`. Enhanced `upgrade.sh` with refined Mesa/LLVM version definitions and streamlined package installation. Fixed `systemd_pid` retrieval logic in `install-desktop.sh` for robustness.

--- linux_files/upgrade.sh
+++ linux_files/upgrade.sh
@@ -23,8 +23,8 @@ sudo rm -f /var/lib/rpm/.rpm.lock
 # Update mesa
 source /etc/os-release
 
-declare -a mesa_version=('23.1.4-2_wsl' '24.2.8-2_wsl_2')
-declare -a llvm_version=('17.0.6' '19.1.7')
+declare -a mesa_version=('23.1.4-4_wsl' '24.2.8-2_wsl_2')
+declare -a llvm_version=('llvm-compat-libs-17.0.6' 'llvm-libs-19.1.7')
 declare -a target_version=('8' '9')
 declare -i length=${#mesa_version[@]}
 
@@ -35,7 +35,7 @@ for ((i = 0; i < length; i++)); do
       sudo dnf -y install 'dnf-command(versionlock)'
       sudo dnf versionlock delete llvm-libs mesa-dri-drivers mesa-libGL mesa-filesystem mesa-libglapi mesa-vdpau-drivers mesa-libEGL mesa-libgbm mesa-libxatracker mesa-vulkan-drivers
       curl -s https://packagecloud.io/install/repositories/whitewaterfoundry/pengwin-enterprise/script.rpm.sh | sudo bash
-      sudo dnf -y install --allowerasing --nogpgcheck llvm-libs-"${llvm_version[i]}" mesa-dri-drivers-"${mesa_version[i]}".el"${target_version[i]}" mesa-libGL-"${mesa_version[i]}".el"${target_version[i]}" mesa-vdpau-drivers-"${mesa_version[i]}".el"${target_version[i]}" mesa-libEGL-"${mesa_version[i]}".el"${target_version[i]}" mesa-libgbm-"${mesa_version[i]}".el"${target_version[i]}" mesa-libxatracker-"${mesa_version[i]}".el"${target_version[i]}" mesa-vulkan-drivers-"${mesa_version[i]}".el"${target_version[i]}" glx-utils libva
+      sudo dnf -y install --allowerasing --nogpgcheck "${llvm_version[i]}" mesa-dri-drivers-"${mesa_version[i]}".el"${target_version[i]}" mesa-libGL-"${mesa_version[i]}".el"${target_version[i]}" mesa-vdpau-drivers-"${mesa_version[i]}".el"${target_version[i]}" mesa-libEGL-"${mesa_version[i]}".el"${target_version[i]}" mesa-libgbm-"${mesa_version[i]}".el"${target_version[i]}" mesa-libxatracker-"${mesa_version[i]}".el"${target_version[i]}" mesa-vulkan-drivers-"${mesa_version[i]}".el"${target_version[i]}" glx-utils libva
       sudo dnf -y install --allowerasing --nogpgcheck libva-utils
       sudo dnf versionlock add llvm-libs mesa-dri-drivers mesa-libGL mesa-filesystem mesa-libglapi mesa-vdpau-drivers mesa-libEGL mesa-libgbm mesa-libxatracker mesa-vulkan-drivers
 
